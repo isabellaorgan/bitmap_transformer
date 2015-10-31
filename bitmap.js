d5d4e1c623cdb8461bf0e6353e6cc234a41cd1ea
Chadwick Bissette <chadwick.bissette@gmail.com>
2015-10-31 15:50:53 -0700

    basic bitmap.js structure to write a new file replacing all of one color with another color

--- bitmap.js
+++ bitmap.js
@@ -1,8 +1,32 @@
-var fs = require('fs');
-var bitmap = fs.readFileSync('bitmap1.bmp');
-console.log(bitmap.toString('utf-8', 0, 2));
+'use strict'
 
-console.log('size ' + bitmap.readUInt32LE(2));
-console.log('pixel data start ' + bitmap.readUInt32LE(10));
-console.log('bit depth: ' + bitmap.readUInt16LE(28));
-console.log('number of colors: ' + bitmap.readUInt32LE(46));
+var fs = require('fs');
+var fileName = 'bitmap1.bmp';
+var transformedFileName = 'bitmap_tranformed.bmp';
+var pixArrOffset = 1078; //Byte offset of pixel array in bitmap
+var bytesPerPixel = 1;
+var pixArrWidth = 100;
+var pixArrHeight = 100;
+var pixArrBytes = pixArrWidth * pixArrHeight * bytesPerPixel;
+var transformedBuffer;
+
+function transform(uint) {
+if (uint === 22) return 12;
+return uint;
+}
+
+fs.readFile(fileName, function (err, data) {
+  if (err) throw err;
+  for (var i = 0; i < pixArrBytes; i++) {
+    var current = data.readUInt8((pixArrOffset) + i);
+    var currentTransformed = transform(current);
+    data.writeUInt8(currentTransformed, (pixArrOffset) + i);
+  }
+  transformedBuffer = data;
+  // return transformedBuffer;
+
+  fs.writeFile('bitmap_tranformed.bmp', transformedBuffer, function (err){
+    if (err) throw err;
+    if (!err) console.log('transformed ' + fileName + ' into ' + transformedFileName + '!');
+  });
+});
